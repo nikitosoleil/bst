7878ab9a2f1f37e83c28656ff2ecd63a125ce686
nikiolei <nikiolei@gmail.com>
2018-05-13 16:01:19 +0300

    Shitty 2-3 tree

--- Bst.cpp
+++ Bst.cpp
@@ -1,5 +1,6 @@
 #include "BstAvl.h"
 #include "BstTreap.h"
+#include "BstTwoThree.h"
 
 #include <iostream>
 #include <set>
@@ -12,7 +13,7 @@ using namespace BST;
 struct Pair
 {
 	int X, Y;
-	static int INF = 1e9+2;
+	static const int INF = 1e9+2;
 	Pair (int x = 0, int y = 0): X(x), Y(y) {}
 	bool operator > (const Pair &P) const
 	{
@@ -30,6 +31,14 @@ struct Pair
 	{
 		return X != P.X;
 	}
+	bool operator >= (const Pair &P) const
+	{
+		return X >= P.X;
+	}
+	bool operator <= (const Pair &P) const
+	{
+		return X <= P.X;
+	}
 	Pair operator + (int N)
 	{
 		return Pair(X+N, Y);
@@ -43,127 +52,216 @@ struct Query
 	Query (int t = 0, int x = 0, int y = 0): Type(t), P(x, y) {}
 };
 
-vector < Query > Queries;
-
 typedef long double ld;
 
 #define random() ( rand()*RAND_MAX + rand() )
 
 int main ()
 {
-	set < Pair > Set;
-	AVL < Pair > Avl;
-	Treap < Pair > Treap;
-	
-	srand(time(0));
-	cout << "Enter the amount of queries and maximum element value: \n";
-	int Size, Value;
-	cin >> Size >> Value;
-	for (int i = 0; i < Size; ++i)
-		Queries.push_back(Query(random()%3, random()%Value, random()%Value));
-	cout << "Generation complete!\n";
-	
-	clock_t Time = clock();
-	for (int i = 0; i < Size; ++i)
+	//while (true)
 	{
-		switch (Queries[i].Type)
+		set < Pair > Set;
+		AVL < Pair > Avl;
+		Treap < Pair > Treap;
+		TwoThree < Pair > TwoThree;
+		
+		/*
+		
+		Pair P(1);
+		TwoThree.Insert(P);
+		TwoThree.Test();
+		cerr << "Size: " << TwoThree.Size() << endl;
+		
+		P = Pair(2);
+		TwoThree.Insert(P);
+		TwoThree.Test();
+		cerr << "Size: " << TwoThree.Size() << endl;
+		
+		P = Pair(3);
+		TwoThree.Insert(P);
+		TwoThree.Test();
+		cerr << "Size: " << TwoThree.Size() << endl;
+		
+		P = Pair(2);
+		TwoThree.Erase(P);
+		TwoThree.Test();
+		cerr << "Size: " << TwoThree.Size() << endl;
+		
+		*/
+		
+		srand(time(0));
+		cout << "Enter the amount of queries and maximum element value: \n";
+		int Size, Value;
+		cin >> Size >> Value;
+		//Size = 30; Value = 10;
+		vector < Query > Queries;
+		for (int i = 0; i < Size; ++i)
 		{
-			case 0:
-				Set.insert(Queries[i].P);
-				break;
-			case 1:
-				Set.erase(Queries[i].P);
-				break;
-			case 2:
+			int a = random()%3;
+			int b = random()%Value, c = random()%Value;
+			cin >> a >> b >> c;
+			Queries.push_back(Query(a, b, c));
+			
+		}
+		cout << "Generation complete!\n";
+		
+		clock_t Time = clock();
+		for (int i = 0; i < Size; ++i)
+		{
+			switch (Queries[i].Type)
 			{
-				auto it = Set.find(Queries[i].P);
-				if (it == Set.end()) Queries[i].Response = -1;
-				else Queries[i].Response = it->Y;
-				break;
+				case 0:
+					Set.insert(Queries[i].P);
+					break;
+				case 1:
+					Set.erase(Queries[i].P);
+					break;
+				case 2:
+				{
+					auto it = Set.find(Queries[i].P);
+					if (it == Set.end()) Queries[i].Response = -1;
+					else Queries[i].Response = it->Y;
+					break;
+				}
 			}
 		}
-	}
-	cout << "Set processing complete!\n";
-	cout << (clock()-Time+0.0)/ld(CLOCKS_PER_SEC) << endl;
-	
-	Time = clock();
-	bool Check = true;
-	for (int i = 0; i < Size; ++i)
-	{
-		switch (Queries[i].Type)
+		cout << "Set processing complete!\n";
+		cout << (clock()-Time+0.0)/ld(CLOCKS_PER_SEC) << endl;
+		
+		Time = clock();
+		bool Check = true;
+		for (int i = 0; i < Size; ++i)
 		{
-			case 0:
-				Avl.Insert(Queries[i].P);
-				break;
-			case 1:
-				Avl.Erase(Queries[i].P);
-				break;
-			case 2:
+			switch (Queries[i].Type)
 			{
-				Pair Response;
-				if (Avl.Find(Queries[i].P, Response))
+				case 0:
+					Avl.Insert(Queries[i].P);
+					break;
+				case 1:
+					Avl.Erase(Queries[i].P);
+					break;
+				case 2:
 				{
-					if (Response.Y != Queries[i].Response)
+					Pair Response;
+					if (Avl.Find(Queries[i].P, Response))
 					{
-						cout << "Attention: " << Response.Y << ' ' << Queries[i].Response << endl;
-						Check = false;
+						if (Response.Y != Queries[i].Response)
+						{
+							cout << "Attention: " << Response.Y << ' ' << Queries[i].Response << endl;
+							Check = false;
+						}
 					}
-				}
-				else
-				{
-					if (Queries[i].Response != -1)
+					else
 					{
-						cout << "Attention: " << Queries[i].Response << endl;
-						Check = false;
+						if (Queries[i].Response != -1)
+						{
+							cout << "Attention: " << Queries[i].Response << endl;
+							Check = false;
+						}
 					}
+					break;
 				}
-				break;
 			}
 		}
-	}
-	cout << "Avl processing complete!\n";
-	cout << (clock()-Time+0.0)/ld(CLOCKS_PER_SEC) << endl;
-	if (Check) cout << "All answers are matching!\n";
-	else cout << "Sorry, there are some mistakes...\n";
-	
-	Time = clock();
-	Check = true;
-	for (int i = 0; i < Size; ++i)
-	{
-		switch (Queries[i].Type)
+		cout << "Avl processing complete!\n";
+		cout << (clock()-Time+0.0)/ld(CLOCKS_PER_SEC) << endl;
+		if (Check) cout << "All answers are matching!\n";
+		else cout << "Sorry, there are some mistakes...\n";
+		
+		Time = clock();
+		Check = true;
+		for (int i = 0; i < Size; ++i)
 		{
-			case 0:
-				Treap.Insert(Queries[i].P, random()%Value);
-				break;
-			case 1:
-				Treap.Erase(Queries[i].P);
-				break;
-			case 2:
-				Pair Response;
-				if (Treap.Find(Queries[i].P, Response))
-				{
-					if (Response.Y != Queries[i].Response)
+			switch (Queries[i].Type)
+			{
+				case 0:
+					Treap.Insert(Queries[i].P, random()%Value);
+					break;
+				case 1:
+					Treap.Erase(Queries[i].P);
+					break;
+				case 2:
+					Pair Response;
+					if (Treap.Find(Queries[i].P, Response))
 					{
-						cout << "Attention: " << Response.Y << ' ' << Queries[i].Response << endl;
-						Check = false;
+						if (Response.Y != Queries[i].Response)
+						{
+							cout << "Attention: " << Response.Y << ' ' << Queries[i].Response << endl;
+							Check = false;
+						}
 					}
-				}
-				else
-				{
-					if (Queries[i].Response != -1)
+					else
 					{
-						cout << "Attention: " << Queries[i].Response << endl;
-						Check = false;
+						if (Queries[i].Response != -1)
+						{
+							cout << "Attention: " << Queries[i].Response << endl;
+							Check = false;
+						}
 					}
+					break;
+			}
+		}
+		cout << "Treap processing complete!\n";
+		cout << (clock()-Time+0.0)/ld(CLOCKS_PER_SEC) << endl;
+		if (Check) cout << "All answers are matching!\n";
+		else cout << "Sorry, there are some mistakes...\n";
+		
+		Time = clock();
+		Check = true;
+		for (int i = 0; i < Size; ++i)
+		{
+			switch (Queries[i].Type)
+			{
+				case 0:
+					TwoThree.Insert(Queries[i].P);
+					break;
+				case 1:
+					TwoThree.Erase(Queries[i].P);
+					break;
+				case 2:
+				{
+					Pair Response;
+					if (TwoThree.Find(Queries[i].P, Response))
+					{
+						if (Response.Y != Queries[i].Response)
+						{
+							cout << "Attention: " << Response.Y << ' ' << Queries[i].Response << endl;
+							Check = false;
+						}
+					}
+					else
+					{
+						if (Queries[i].Response != -1)
+						{
+							if (Check)
+							{
+								for (int j = 0; j <= i; ++j)
+									cout << Queries[j].Type << ' ' << Queries[j].P.X << ' ' << Queries[j].P.Y << endl;
+							}
+							cout << Response.X << ' ' << Response.Y << endl;
+							cout << "Attention: " << Queries[i].Response << endl;
+							Check = false;
+						}
+					}
+					break;
 				}
-				break;
+			}
+			cout << Queries[i].Type << ' ' << Queries[i].P.X << ' ' << Queries[i].P.Y << endl;
+			TwoThree.Test();
+			cout << TwoThree.Size() << endl;
+			//TwoThree.Test();
+		}
+		cout << "TwoThree processing complete!\n";
+		cout << (clock()-Time+0.0)/ld(CLOCKS_PER_SEC) << endl;
+		cout << "Sizes are: " << Set.size() << ", " << Avl.Size() << ", " << Treap.Size() << " and " << TwoThree.Size() << endl;
+		if (Check && Set.size() == Avl.Size() && Set.size() == Treap.Size() && Set.size() == TwoThree.Size()) cout << "All answers are matching!\n";
+		else
+		{
+			cout << "Sorry, there are some mistakes...\n";
+			for (auto it: Queries)
+				cout << it.Type << ' ' << it.P.X << ' ' << it.P.Y << endl;
+			cin >> Check;
 		}
 	}
-	cout << "Treap processing complete!\n";
-	cout << (clock()-Time+0.0)/ld(CLOCKS_PER_SEC) << endl;
-	if (Check || Set.size() != Avl.Size() || Set.size() != Treap.Size()) cout << "All answers are matching!\n";
-	else cout << "Sorry, there are some mistakes...\n";
-	cout << "Sizes are: " << Set.size() << ", " << Avl.Size() << " and " << Treap.Size() << endl;
-	system("pause");
 	return 0;
 }
--- BstTwoThree.h
+++ BstTwoThree.h
@@ -1,8 +1,12 @@
 #pragma once
 
 #include <vector>
+#include <iostream> /// TODO: GTFO
+#include <cassert>
 
 using namespace std;
+
+#define max(a, b) ((a>b)?a:b)
 
 namespace BST
 {
@@ -17,73 +21,233 @@ namespace BST
 			Node **To;
 			Node ()
 			{
-				Value = -Target::INF;
+				Value = Target(-Target::INF, 0);
 				Max = new Target[4];
 				To = new Node *[4];
+				Degree = 0;
 				for (int i = 0; i < 4; ++i)
 				{
-					Max[3] = Target::INF;
-					To[3] = 0;
+					Max[i] = Target(Target::INF, 0);
+					To[i] = 0;
 				}
 			}
 			~Node ()
 			{
 				delete Max;
+				delete To;
 			}
 		} *Root;
 		
-		void Update (Node *V)
+		
+		void Update(Node *V)
+		{
+			if(V && V->Degree)
+			{
+				V->Value = -Target::INF;
+				for(int i=0; i<V->Degree; ++i)
+				{
+					V->Max[i] = V->To[i]->Value;
+					V->Value = max(V->Value, V->Max[i]);
+				}
+				for (int i = V->Degree; i < 4; ++i)
+				{
+					V->Max[i] = Target::INF;
+					V->To[i] = 0;
+				}
+			}
+		}
+		void Append (Node *V, Node *U)
+		{
+			int i;
+			for (i = V->Degree-1; i >= 0 && V->Max[i] > U->Value; --i)
+			{
+				V->Max[i+1] = V->Max[i];
+				V->To[i+1] = V->To[i];
+			}
+			V->To[i+1] = U;
+			V->Max[i+1] = U->Value;
+			V->Value = max(V->Value, U->Value);
+			++V->Degree;
+		}
+		void Reduce (Node *V, Node *P)
 		{
 			if (V->Degree == 4)
 			{
 				V->Degree = 2;
-				Node **N = new Node *[2];
+				Node *New = new Node;
+				New->Degree = 2;
 				for (int i = 0; i <= 1; ++i)
 				{
-					N[i] = new Node;
-					N[i]->Degree = 2;
-					for (int j = 0; j <= 1; ++j)
-					{
-						N[i]->Max[j] = V->Max[i*2+j];
-						N[i]->To[j] = V->To[i*2+j];
-						N[i]->Value = max(N[i]->Value, N[i]->Max[j]);
-					}
-					V->Max[i] = N[i]->Value;
-					V->To[i] = N[i];
+					New->To[i] = V->To[i];
+					New->Max[i] = V->Max[i];
+					V->To[i] = V->To[i+2];
+					V->Max[i] = V->Max[i+2];
+					V->To[i+2] = 0;
+					V->Max[i+2] = Target::INF;
 				}
+				New->Value = max(New->Max[0], New->Max[1]);
+				if (!P)
+				{
+					Root = new Node;
+					Root->To[0] = New;
+					Root->To[1] = V;
+					Root->Max[0] = New->Value;
+					Root->Max[1] = V->Value;
+					Root->Degree = 2;
+					Root->Value = max(Root->Max[0], Root->Max[1]);
+				}
+				else
+					Append(P, New);
 			}
 		}
 		void Insert (Node *V, Node *P, Target &Object)
 		{
-			if (V->Max[1] < Object)
+			if (!V)
 			{
-				Insert(V->To[2], V, Object);
+				Root = new Node;
+				Root->Value = Object;
+				Root->Degree = 0;
+			}
+			else
+			{
+				if (V->Degree > 2 && V->Max[1] < Object && V->To[2])
+				{
+					Insert(V->To[2], V, Object);
+					Reduce(V, P);
+				}
+				else if (V->Degree > 1 && V->Max[0] < Object && V->To[1])
+				{
+					Insert(V->To[1], V, Object);
+					Reduce(V, P);
+				}
+				else if (V->Degree > 01 && V->To[0])
+				{
+					Insert(V->To[0], V, Object);
+					Reduce(V, P);
+				}
+				else if (V->Value != Object)
+				{
+					Node *New = new Node;
+					New->Value = Object;
+					New->Degree = 0;
+					if (!P)
+					{
+						if (New->Value > V->Value)
+							swap(New, V);
+						Root = new Node;
+						Root->To[0] = New;
+						Root->To[1] = V;
+						Root->Max[0] = New->Value;
+						Root->Max[1] = V->Value;
+						Root->Degree = 2;
+						Root->Value = max(Root->Max[0], Root->Max[1]);
+					}
+					else
+						Append(P, New);
+				}
 				Update(V);
 			}
-			else if (V->Max[0] < Object)
-			{
-				Insert(V->To[1], V, Object);
-				Update(V);
-			}
-			else if (V->To[0])
-			{
-				Insert(V->To[0], V, Object);
-				Update(V);
-			}
-			else if (V->Max[0] == Object)
+		}
+		bool Erase (Node *V, Node *P, Node *GP, Target &Object)
+		{
+			if (!V)
+				return false;
+			bool Ret = false;
+			if (V->Degree > 2 && V->Max[1] < Object && V->To[2])
+				Ret = Erase(V->To[2], V, P, Object);
+			else if (V->Degree > 1 && V->Max[0] < Object && V->To[1])
+				Ret = Erase(V->To[1], V, P, Object);
+			else if (V->Degree > 0 && V->To[0])
+				Ret = Erase(V->To[0], V, P, Object);
+			else if (V->Value == Object)
+				Ret = true;
+			
+			if (Ret && V->Degree == 0)
 			{
 				if (!P)
-					P = new Node;
-				int i;
-				for (i = P->Degree-1; i >= 0 && P->Max[i] > Object; --i)
-					P->Max[i+1] = P->Max[i];
-				P->To[i+1] = P->To[i];
-				P->To[i+1] = new Node;
-				P->Max[i+1] = P->To[i+1]->Value = Object;
-				P->Value = max(P->Value, Object);
-				++P->Degree;
-				P->To[i+1]->Degree = 1;
+					Root = 0;
+				else if (P->Degree == 3)
+				{
+					for (int i = 0; i < P->Degree; ++i)
+						if (P->To[i] == V)
+						{
+							for (int j = i; j < P->Degree; ++j)
+							{
+								P->To[j] = P->To[j+1];
+								P->Max[j] = P->Max[j+1];
+							}
+							break;
+						}
+					P->To[P->Degree-1]=0;
+					P->Max[P->Degree-1] = Target(Target::INF, 0);
+					--P->Degree;
+				}
+				else
+				{
+					Node *B = 0;
+					for (int i = 0; i < P->Degree; ++i)
+						if (P->To[i] != V)
+							B = P->To[i];
+					if (!GP)
+					{
+						Root = B;
+						P->Degree = 123456789;
+					}
+					else
+					{
+						Node *NP = 0;
+						for (int i = 0; i < GP->Degree; ++i)
+							if (GP->To[i] == P)
+							{
+								if(i>0)
+									NP = GP->To[i-1];
+								else
+									NP = GP->To[i+1];
+								/*
+								for (int j = i; j < GP->Degree; ++j)
+								{
+									GP->To[j] = GP->To[j+1];
+									GP->Max[j] = GP->Max[j+1];
+								}
+								*/
+								break;
+							}
+						// GP->To[GP->Degree-1] = 0;
+						// GP->Max[GP->Degree-1] = Target(Target::INF, 0);
+						Append(NP, B);
+						Reduce(NP, GP);
+						for(int i=0; i<2; ++i)
+						{
+							P->Max[i] = Target::INF;
+							P->To[i] = 0;
+						}
+						P->Degree = 0;
+					}
+				}
+				delete V;
 			}
+			if(V->Degree == 123456789)
+				delete V;
+			//Update(V);
+			
+			return Ret;
+		}
+		void Clear (Node *V)
+		{
+			if (!V)
+				return;
+			for (int i = 0; i < V->Degree; ++i)
+				Clear(V->To[i]);
+			delete V;
+		}
+		int Size (Node *V)
+		{
+			if (!V)
+				return 0;
+			int Ret = 0;
+			for (int i = 0; i < V->Degree; ++i)
+				Ret += Size(V->To[i]);
+			return max(Ret, 1);
 		}
 	public:
 		TwoThree ()
@@ -92,15 +256,31 @@ namespace BST
 		}
 		~TwoThree ()
 		{
-			/// FUCK IT
+			Clear();
+		}
+		void Clear ()
+		{
+			Clear(Root);
+		}
+		int Size ()
+		{
+			return Size(Root);
+		}
+		void Insert (Target &Object)
+		{
+			Insert(Root, 0, Object);
+		}
+		bool Erase (Target &Object)
+		{
+			return Erase(Root, 0, 0, Object);
 		}
 		bool Find (Target &Object, Target &Result)
 		{
 			Node *V = Root;
-			while (true)
-				if (V->Max[1] < Object)
+			while (V)
+				if (V->Max[1] < Object && V->To[2])
 					V = V->To[2];
-				else if (V->Max[0] < Object)
+				else if (V->Max[0] < Object && V->To[1])
 					V = V->To[1];
 				else if (V->To[0])
 					V = V->To[0];
@@ -111,6 +291,23 @@ namespace BST
 				}
 				else
 					return false;
+			return false;
+		}
+		void Test (Node *V)
+		{
+			if(!V)
+				return;
+			cout << "V: " << V->Degree << ' ' << V->Value.X << ' ' << V->Value.Y << endl;
+			for (int i = 0; i < V->Degree; ++i)
+				cout << V->Max[i].X << ' ' << V->Max[i].Y << '\t';
+			cout << endl;
+			for (int i = 0; i < V->Degree; ++i)
+				Test(V->To[i]);
+		}
+		void Test () /// TODO: GTFO
+		{
+			cout << "Test\n";
+			Test(Root);
 		}
 	};
 }
