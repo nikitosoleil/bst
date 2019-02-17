1ac3be17de763c22765cf2b52e7d93652dbf08f1
nikitosoleil <nikiolei@gmail.com>
2019-02-18 01:58:38 +0200

    First RB-tree

--- Bst.cpp
+++ Bst.cpp
@@ -1,6 +1,7 @@
 #include "BstAvl.h"
 #include "BstTreap.h"
 #include "BstTwoThree.h"
+#include "BstRbt.h"
 
 #include <iostream>
 #include <set>
@@ -13,35 +14,35 @@ using namespace BST;
 struct Pair
 {
 	int X, Y;
-	static const int INF = 1e9+2;
-	Pair (int x = 0, int y = 0): X(x), Y(y) {}
-	bool operator > (const Pair &P) const
+	static const int INF = 1e9 + 2;
+	Pair(int x = 0, int y = 0) : X(x), Y(y) {}
+	bool operator>(const Pair &P) const
 	{
 		return X > P.X;
 	}
-	bool operator < (const Pair &P) const
+	bool operator<(const Pair &P) const
 	{
 		return X < P.X;
 	}
-	bool operator == (const Pair &P) const
+	bool operator==(const Pair &P) const
 	{
 		return X == P.X;
 	}
-	bool operator != (const Pair &P) const
+	bool operator!=(const Pair &P) const
 	{
 		return X != P.X;
 	}
-	bool operator >= (const Pair &P) const
+	bool operator>=(const Pair &P) const
 	{
 		return X >= P.X;
 	}
-	bool operator <= (const Pair &P) const
+	bool operator<=(const Pair &P) const
 	{
 		return X <= P.X;
 	}
-	Pair operator + (int N)
+	Pair operator+(int N)
 	{
-		return Pair(X+N, Y);
+		return Pair(X + N, Y);
 	}
 };
 
@@ -49,63 +50,102 @@ struct Query
 {
 	int Type, Response;
 	Pair P;
-	Query (int t = 0, int x = 0, int y = 0): Type(t), P(x, y) {}
+	Query(int t = 0, int x = 0, int y = 0) : Type(t), P(x, y) {}
 };
 
 typedef long double ld;
 
 #define random() ( rand()*RAND_MAX + rand() )
 
-int main ()
+int main()
 {
 	srand(time(0));
 	set < Pair > Set;
 	AVL < Pair > Avl;
 	Treap < Pair > Treap;
 	TwoThree < Pair > TwoThree;
-	
+	RBT < Pair > RBT;
+
 	cout << "Enter the amount of queries and maximum element value: \n";
 	int Size, Value;
-	cin >> Size >> Value;
+	// cin >> Size >> Value;
+	Size = 100000, Value = 100000;
 	vector < Query > Queries;
 	for (int i = 0; i < Size; ++i)
-		Queries.push_back(Query(random()%3, random()%Value, random()%Value));
+		Queries.push_back(Query((random()%2)*2, random()%Value, random()%Value));
 	cout << "Generation complete!\n";
-	
+
 	clock_t Time = clock();
 	for (int i = 0; i < Size; ++i)
 	{
 		switch (Queries[i].Type)
 		{
-			case 0:
-				Set.insert(Queries[i].P);
+			case 0: Set.insert(Queries[i].P);
 				break;
-			case 1:
-				Set.erase(Queries[i].P);
+			case 1: Set.erase(Queries[i].P);
 				break;
 			case 2:
 			{
 				auto it = Set.find(Queries[i].P);
-				if (it == Set.end()) Queries[i].Response = -1;
-				else Queries[i].Response = it->Y;
+				if (it == Set.end())
+					Queries[i].Response = -1;
+				else
+					Queries[i].Response = it->Y;
 				break;
 			}
 		}
 	}
 	cout << "Set processing complete!\n";
-	cout << (clock()-Time+0.0)/ld(CLOCKS_PER_SEC) << endl;
-	
+	cout << (clock() - Time + 0.0)/ld(CLOCKS_PER_SEC) << endl;
+
 	Time = clock();
 	bool Check = true;
 	for (int i = 0; i < Size; ++i)
 	{
 		switch (Queries[i].Type)
 		{
-			case 0:
-				Avl.Insert(Queries[i].P);
+			case 0: RBT.insert(Queries[i].P);
 				break;
-			case 1:
-				Avl.Erase(Queries[i].P);
+			case 1: return 1;
+			case 2:
+			{
+				Pair Response;
+				if (RBT.find(Queries[i].P, Response))
+				{
+					if (Response.Y != Queries[i].Response)
+					{
+						cout << "Attention: " << Response.Y << ' ' << Queries[i].Response << endl;
+						Check = false;
+					}
+				}
+				else
+				{
+					if (Queries[i].Response != -1)
+					{
+						cout << "Attention: " << Queries[i].Response << endl;
+						Check = false;
+					}
+				}
+				break;
+			}
+		}
+	}
+	cout << "RBT processing complete!\n";
+	cout << (clock() - Time + 0.0)/ld(CLOCKS_PER_SEC) << endl;
+	if (Check)
+		cout << "All answers are matching!\n";
+	else
+		cout << "Sorry, there are some mistakes...\n";
+
+	Time = clock();
+	Check = true;
+	for (int i = 0; i < Size; ++i)
+	{
+		switch (Queries[i].Type)
+		{
+			case 0: Avl.Insert(Queries[i].P);
+				break;
+			case 1: Avl.Erase(Queries[i].P);
 				break;
 			case 2:
 			{
@@ -131,24 +171,23 @@ int main ()
 		}
 	}
 	cout << "Avl processing complete!\n";
-	cout << (clock()-Time+0.0)/ld(CLOCKS_PER_SEC) << endl;
-	if (Check) cout << "All answers are matching!\n";
-	else cout << "Sorry, there are some mistakes...\n";
-	
+	cout << (clock() - Time + 0.0)/ld(CLOCKS_PER_SEC) << endl;
+	if (Check)
+		cout << "All answers are matching!\n";
+	else
+		cout << "Sorry, there are some mistakes...\n";
+
 	Time = clock();
 	Check = true;
 	for (int i = 0; i < Size; ++i)
 	{
 		switch (Queries[i].Type)
 		{
-			case 0:
-				Treap.Insert(Queries[i].P, random()%Value);
+			case 0: Treap.Insert(Queries[i].P, random()%Value);
 				break;
-			case 1:
-				Treap.Erase(Queries[i].P);
+			case 1: Treap.Erase(Queries[i].P);
 				break;
-			case 2:
-				Pair Response;
+			case 2: Pair Response;
 				if (Treap.Find(Queries[i].P, Response))
 				{
 					if (Response.Y != Queries[i].Response)
@@ -169,21 +208,21 @@ int main ()
 		}
 	}
 	cout << "Treap processing complete!\n";
-	cout << (clock()-Time+0.0)/ld(CLOCKS_PER_SEC) << endl;
-	if (Check) cout << "All answers are matching!\n";
-	else cout << "Sorry, there are some mistakes...\n";
-	
+	cout << (clock() - Time + 0.0)/ld(CLOCKS_PER_SEC) << endl;
+	if (Check)
+		cout << "All answers are matching!\n";
+	else
+		cout << "Sorry, there are some mistakes...\n";
+
 	Time = clock();
 	Check = true;
 	for (int i = 0; i < Size; ++i)
 	{
 		switch (Queries[i].Type)
 		{
-			case 0:
-				TwoThree.Insert(Queries[i].P);
+			case 0: TwoThree.Insert(Queries[i].P);
 				break;
-			case 1:
-				TwoThree.Erase(Queries[i].P);
+			case 1: TwoThree.Erase(Queries[i].P);
 				break;
 			case 2:
 			{
@@ -209,9 +248,11 @@ int main ()
 		}
 	}
 	cout << "TwoThree processing complete!\n";
-	cout << (clock()-Time+0.0)/ld(CLOCKS_PER_SEC) << endl;
-	cout << "Sizes are: " << Set.size() << ", " << Avl.Size() << ", " << Treap.Size() << " and " << TwoThree.Size() << endl;
-	if (Check && Set.size() == Avl.Size() && Set.size() == Treap.Size() && Set.size() == TwoThree.Size()) cout << "All answers are matching!\n";
-	else cout << "Sorry, there are some mistakes...\n";
+	cout << (clock() - Time + 0.0)/ld(CLOCKS_PER_SEC) << endl;
+	cout << "Sizes are: " << Set.size() << ", " << RBT.size() << ", " << Avl.Size() << ", " << Treap.Size() << " and " << TwoThree.Size() << endl;
+	if (Check && Set.size() == RBT.size() && Set.size() == Avl.Size() && Set.size() == Treap.Size() && Set.size() == TwoThree.Size())
+		cout << "All answers are matching!\n";
+	else
+		cout << "Sorry, there are some mistakes...\n";
 	return 0;
 }
---
+++ BstRbt.h
@@ -0,0 +1,144 @@
+#pragma once
+
+using namespace std;
+
+namespace BST
+{
+template < class T >
+class RBT
+{
+private:
+	enum Color { red, black };
+	struct Node
+	{
+		T v;
+		Color c;
+		Node *l = nullptr, *r = nullptr;
+		Node(T v, Color c) : v(v), c(c) {}
+	} *root = nullptr;
+	void lr(Node *&n) // left rotation
+	{
+		Node *t = n;
+		n = n->r;
+		t->r = n->l;
+		n->l = t;
+	}
+	void rr(Node *&n) // right rotation
+	{
+		Node *t = n;
+		n = n->l;
+		t->l = n->r;
+		n->r = t;
+	}
+	Color c(Node *&n)
+	{
+		return n ? n->c : black;
+	}
+	void insert(Node *&n, Node *p, Node *b, T v)
+	{
+		if (!n)
+			n = new Node(v, red);
+		else if (v < n->v)
+		{
+			insert(n->l, n, n->r, v);
+			if (n->c == red && n->l->c == red)
+			{
+				if (c(b) == red)
+				{
+					n->c = black;
+					b->c = black;
+					p->c = red;
+				}
+				else
+				{
+					if (p && p->r == n)
+						rr(n);
+				}
+			}
+			if (c(n->l) == red && c(n->l->l) == red)
+			{
+				rr(n);
+				n->c = black;
+				n->r->c = red;
+			}
+		}
+		else if (v > n->v)
+		{
+			insert(n->r, n, n->l, v);
+			if (n->c == red && n->r->c == red)
+			{
+				if (c(b) == red)
+				{
+					n->c = black;
+					b->c = black;
+					p->c = red;
+				}
+				else
+				{
+					if (p && p->l == n)
+						lr(n);
+				}
+			}
+			if (c(n->r) == red && c(n->r->r) == red)
+			{
+				lr(n);
+				n->c = black;
+				n->l->c = red;
+			}
+
+		}
+	}
+	void clear(Node *n)
+	{
+		if (n)
+		{
+			clear(n->l);
+			clear(n->r);
+			delete n;
+		}
+	}
+	int size(Node *n)
+	{
+		if (n)
+			return size(n->l) + size(n->r) + 1;
+		else
+			return 0;
+	}
+public:
+	RBT() {}
+	~RBT()
+	{
+		clear();
+	}
+	void insert(T &value)
+	{
+		insert(root, nullptr, nullptr, value);
+	}
+	void clear()
+	{
+		clear(root);
+	}
+	int size()
+	{
+		return size(root);
+	}
+	bool find(T &value, T &result)
+	{
+		Node *n = root;
+		while (true)
+		{
+			if (!n)
+				return false;
+			else if (n->v == value)
+			{
+				result = n->v;
+				return true;
+			}
+			else if (value <= n->v)
+				n = n->l;
+			else
+				n = n->r;
+		}
+	}
+};
+}
